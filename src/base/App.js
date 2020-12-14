bfde0c332647bc07dc5dca452f78ff4645c31ced
Alexander Droste <mail@alexanderdroste.de>
2020-12-14 02:28:57 +0100

    page unload hint

--- src/base/App.js
+++ src/base/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import { ModelerScreen } from '../screens/ModelerScreen';
 import { ScreenEnum } from '../screens/ScreenEnum';
@@ -8,6 +8,16 @@ import { appContext } from './AppContextProvider';
 function App() {
 
     const { screen } = useContext(appContext);
+
+    useEffect(() => {
+        window.onbeforeunload = (e) => {
+            // custom messages only supported in older browsers
+            const msg = "Wollen Sie den Modeler wirklich verlassen? Alle ungespeicherten Änderungen gehen verloren!";
+            e.preventDefault();
+            e.returnValue = msg;
+            return msg;
+        }
+    }, []);
 
     if (screen === ScreenEnum.MODELER)
         return <ModelerScreen />
