23650977123f68822268e996eb9820ee520030b3
Alexander Droste <mail@alexanderdroste.de>
2020-12-20 14:36:37 +0100

    feat: hint for issues in save dialog

--- src/modeler/SaveDialog.js
+++ src/modeler/SaveDialog.js
@@ -1,41 +1,32 @@
-import { Button, Form, Input, Modal, Space } from 'antd';
+import { Alert, Button, Form, Input, Modal, Space } from 'antd';
 import { CloudOutlined, CloudUploadOutlined, DesktopOutlined, DownloadOutlined, SendOutlined } from '@ant-design/icons';
 import React, { useCallback, useContext, useEffect, useState } from 'react';
 
 import { appContext } from '../base/AppContextProvider';
 import { getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
-import { is } from '../meta-model/rules/util';
 import { modelerContext } from './ModelerContextProvider';
 import styles from './SaveDialog.module.css';
+import { useIssues } from './useIssues';
 
 export function SaveDialog({
     onClose,
 }) {
     const { exportWorkflow } = useContext(appContext);
-    const { elementRegistry, eventBus, getXml } = useContext(modelerContext);
-    const [processElement, setProcessElement] = useState();
+    const { canvas, eventBus, getXml } = useContext(modelerContext);
+    const [rootElement, setRootElement] = useState();
     const [form] = Form.useForm();
 
+    const issues = useIssues();
+    const hasIssues = Object.keys(issues).length !== 0;
+
     useEffect(() => {
-        const processElement = elementRegistry.find(el => is(getBusinessObject(el), 'bpmn:Process'));
-        setProcessElement(processElement);
-
-        if (!processElement) {
-            Modal.error({
-                title: 'Fehler',
-                content: 'bpmn:Process ist kein unreferenziertes Root Element. Workflow kann nicht gespeichert werden.',
-                onOk() {
-                    onClose();
-                }
-            });
-            return;
-        }
-
-        const bo = getBusinessObject(processElement);
+        const rootElement = canvas.getRootElement(); 
+        setRootElement(rootElement);
+        const bo = getBusinessObject(rootElement);
         form.setFieldsValue({
-            name: bo.name,
+            name: bo.name || "",
         });
-    }, [elementRegistry, form, onClose])
+    }, [canvas, form, onClose])
 
     const handleDownload = useCallback(async () => {
         const xml = await getXml();
@@ -56,13 +47,13 @@ export function SaveDialog({
 
     const handleValuesChange = useCallback(values => {
         if (values.hasOwnProperty('name')) {
-                const bo = getBusinessObject(processElement);
-                bo.set('name', values.name);
-                eventBus.fire('elements.changed', { elements: [processElement] });
+            const bo = getBusinessObject(rootElement);
+            bo.set('name', values.name);
+            eventBus.fire('elements.changed', { elements: [rootElement] });
         }
-    }, [eventBus, processElement]);
+    }, [eventBus, rootElement]);
 
-    if (!processElement)
+    if (!rootElement)
         return null;
 
     return (
@@ -73,65 +64,81 @@ export function SaveDialog({
             onCancel={onClose}
             footer={null}
         >
-            <Form
-                form={form}
-                layout='vertical'
-                onValuesChange={handleValuesChange}
-            >
-                <Form.Item
-                    name="name"
-                    label="Name des Workflows"
-                    rules={[
-                        { required: true, message: "Name erforderlich" }
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-            </Form>
+            <Space className={styles.wrapper} direction="vertical">
 
-            <Space 
-                className={styles.buttons} 
-                direction="vertical" 
-                align="center"
-            >
-                <div className={styles.group}>
-                    <CloudOutlined />&nbsp;&nbsp;Online
+                <Form
+                    form={form}
+                    layout='vertical'
+                    onValuesChange={handleValuesChange}
+                >
+                    <Form.Item
+                        name="name"
+                        label="Name des Workflows"
+                        rules={[
+                            { required: true, message: "Name erforderlich" }
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Form>
+
+                {hasIssues &&
+                    <Alert
+                        type="warning"
+                        description={
+                            <div>
+                                Der Workflow enthält unbeseitigte Fehler/Warnungen.<br />
+                                Die Pflegebrille kann diesen Workflow <strong>nicht ausführen!</strong>
+                            </div>
+                        }
+                        showIcon
+                    />
+                }
+
+                <Space
+                    className={styles.buttons}
+                    direction="vertical"
+                    align="center"
+                >
+                    <div className={styles.group}>
+                        <CloudOutlined />&nbsp;&nbsp;Online
                 </div>
 
-                <Space>
-                    <Button
-                        onClick={() => alert('Cloud Speicher nicht verfügbar.')}
-                        icon={<CloudUploadOutlined />}
-                        shape="round"
-                        type="primary"
-                        disabled
-                    >
-                        In Cloud Speichern
+                    <Space>
+                        <Button
+                            onClick={() => alert('Cloud Speicher nicht verfügbar.')}
+                            icon={<CloudUploadOutlined />}
+                            shape="round"
+                            type="primary"
+                            disabled
+                        >
+                            In Cloud Speichern
                     </Button>
 
-                    <Button
-                        onClick={() => alert('Cloud Speicher nicht verfügbar.')}
-                        icon={<SendOutlined />}
-                        shape="round"
-                        disabled
-                    >
-                        Veröffentlichen
+                        <Button
+                            onClick={() => alert('Cloud Speicher nicht verfügbar.')}
+                            icon={<SendOutlined />}
+                            shape="round"
+                            disabled
+                        >
+                            Veröffentlichen
                     </Button>
-                </Space>
+                    </Space>
 
-                <div></div>
+                    <div></div>
 
-                <div className={styles.group}>
-                    <DesktopOutlined />&nbsp;&nbsp;Lokal
+                    <div className={styles.group}>
+                        <DesktopOutlined />&nbsp;&nbsp;Lokal
                 </div>
 
-                <Button
-                    onClick={handleDownload}
-                    icon={<DownloadOutlined />}
-                    shape="round"
-                >
-                    Workflow herunterladen
+                    <Button
+                        onClick={handleDownload}
+                        icon={<DownloadOutlined />}
+                        shape="round"
+                    >
+                        Workflow herunterladen
                 </Button>
+                </Space>
             </Space>
         </Modal>
     );
