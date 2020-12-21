77365f3def7725528d8aeb81a4d4da36090a5c5a
Alexander Droste <mail@alexanderdroste.de>
2020-12-21 02:44:32 +0100

    chore: remove obsolete ExtensionFunctionSelect

--- src/util.js
+++ src/util.js
@@ -1,18 +1,6 @@
 import { getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
 import { isAny } from './meta-model/rules/util';
 import { modelBindings } from './meta-model/modelBindings';
-import { pbModdle } from './meta-model/pbModdle';
-
-export function getAllowedFunctionsForType(type) {
-    const types = pbModdle.types.filter(t => (
-        t.superClass?.includes('Function')
-        && t.meta?.allowedIn?.includes(type)
-    ));
-    return types.map(t => ({
-        label: t.meta.title || t.name,
-        value: `${pbModdle.prefix}:${t.name}`,
-    }));
-}
 
 export function getModelBindingsForElement(element) {
     const bo = getBusinessObject(element);
