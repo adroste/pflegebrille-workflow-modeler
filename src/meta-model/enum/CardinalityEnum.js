438204fd7bf07022e8eba1e5384fb31ea032eec3
Alexander Droste <mail@alexanderdroste.de>
2020-12-27 22:11:26 +0100

    refactor: sort CardinalityEnum

--- src/meta-model/enum/CardinalityEnum.js
+++ src/meta-model/enum/CardinalityEnum.js
@@ -1,14 +1,14 @@
 // NEVER change the enum values
 export const CardinalityEnum = Object.freeze({
-    ONE: '1',
     ONE_TO_INFINITY: '1_infinity',
-    ZERO_TO_ONE: '0_1',
+    ONE: '1',
     ZERO_TO_INFINITY: '0_infinity',
+    ZERO_TO_ONE: '0_1',
 });
 
 export const CardinalityLabels = {
-    [CardinalityEnum.ONE]: 'genau ein Element',
     [CardinalityEnum.ONE_TO_INFINITY]: '1 bis ∞ Elemente',
-    [CardinalityEnum.ZERO_TO_ONE]: '0 oder 1 Element',
+    [CardinalityEnum.ONE]: 'genau ein Element',
     [CardinalityEnum.ZERO_TO_INFINITY]: '0 bis ∞ Elemente',
+    [CardinalityEnum.ZERO_TO_ONE]: '0 oder 1 Element',
 };
