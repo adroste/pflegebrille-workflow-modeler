e0cb2f17d63341912148aff8c272de8f80b539c2
Alexander Droste <mail@alexanderdroste.de>
2020-12-28 17:58:10 +0100

    refactor: no unused assets rule to use afterCheck callback

--- src/meta-model/rules/noUnusedAssets.js
+++ src/meta-model/rules/noUnusedAssets.js
@@ -8,34 +8,25 @@ export const noUnusedAssets = () => ({
         const assets = [];
         const refs = [];
 
-        function updateReport(reporter) {
-            const unused = assets.filter(({ id }) => refs.every(({ refId }) => refId !== id));
-            reporter.messages = [];
-            if (unused.length) {
-                unused.forEach(asset => {
-                    reporter.report(
-                        asset.id,
-                        `Asset "${asset.name}" ist unbenutzt.`
-                    );
-                });
-            }
+        function afterCheck(node, reporter) {
+            const unused = assets.filter(({ id }) => 
+                refs.every(({ refId }) => refId !== id));
+            unused.forEach(asset => {
+                reporter.report(
+                    asset.id,
+                    `Asset "${asset.name}" ist unbenutzt.`
+                );
+            });
         }
 
         function check(node, reporter) {
-
             if (is(node, 'pb:Assets')) {
                 assets.push(...node.get('assets'));
-                updateReport(reporter);
-                return;
-            }
-
-            if (is(node, 'pb:AssetRef')) {
+            } else if (is(node, 'pb:AssetRef')) {
                 refs.push(node);
-                updateReport(reporter);
-                return;
             }
         }
 
-        return { check };
+        return { check, afterCheck };
     }
 });
