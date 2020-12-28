88a73da44d606bbd4289376a4037670e49908d89
Alexander Droste <mail@alexanderdroste.de>
2020-12-28 17:49:56 +0100

    feat: afterCheck callback for rule instances

--- src/linterConfig.js
+++ src/linterConfig.js
@@ -19,7 +19,33 @@ function resolveRule(pkg, ruleName) {
         throw new Error('cannot resolve rule <' + pkg + '/' + ruleName + '>');
     }
 
-    return () => factory(rule.binding);
+    return () => {
+        const ruleInstance = factory(rule.binding);
+
+        // dirty patch until resolved: https://github.com/bpmn-io/bpmnlint/issues/52
+        if (ruleInstance.afterCheck) {
+            const _check = ruleInstance.check;
+            ruleInstance.check = (node, reporter) => {
+
+                if (!reporter._messages) {
+                    reporter._messages = [];
+                    reporter.report = (id, message) => {
+                        reporter._messages.push({ id, message });
+                    }
+                    Object.defineProperty(reporter, 'messages', {
+                        get: () => {
+                            ruleInstance.afterCheck(node, reporter);
+                            return reporter._messages;
+                        },
+                    });
+                }
+                
+                _check(node, reporter);
+            }
+        }
+
+        return ruleInstance;
+    };
 }
 
 function resolveConfig(pkg, configName) {
